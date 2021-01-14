941df90b75ab194fce669ce8222be0779e2637b6
vaspap1790 <vaspap1790@gmail.com>
2021-01-14 07:55:16 +0200

    cleaning vol2

--- js/custom.js
+++ js/custom.js
@@ -152,28 +152,6 @@ $(document).ready(function () {
         }
     });
 
-    $("#team-slider").owlCarousel({ //owlCarousel settings
-        items: 5, //by default there are 3 slides display.
-        autoplay: true, //the slides will change automatically.
-        smartSpeed: 700, //speed of changing wil be 700
-        loop: true, //infinite loop; after the last slide, first slide starts
-        autoplayHoverPause: true, //when you put mouse over Carousel, slide changing will stop
-        responsive: { //responsiveness as screen size changes
-            // min-width: 0px
-            0: {
-                items: 1 //on devices with width 0 to 579px show 1 slide
-            },
-            // min-width: 579px
-            576: {
-                items: 3 //on devices with width 579px to 768px show show 2 slides
-            },
-            // min-width: 768px
-            768: {
-                items: 5 //on devices with width 768px and above show 3 slides 
-            }
-        }
-    });
-
     // Populate countries in settings modal
     $.ajax({
         type: "POST",
